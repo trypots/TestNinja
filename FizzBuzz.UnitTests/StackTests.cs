7b93a7631223fad12306366c5394ec80c35c2dd8
David Pecot <dp@northernocean.net>
2019-01-19 19:08:08 -0500

    Stack Tests
    My code is a little different from Mosh. Looks close though and covers the same basic cases.

---
+++ FizzBuzz.UnitTests/StackTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace FizzBuzz.UnitTests
+{
+	[TestFixture]
+	public class StackTests
+	{
+
+		Stack<string> _stack;
+		
+		[SetUp]
+		public void Setup()
+		{
+			_stack = new Stack<string>();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			_stack = null;
+		}
+
+		[Test]
+		public void Stack_PushNull_ReturnsArgumentNullException()
+		{
+			Assert.That(() => _stack.Push(null), Throws.Exception.TypeOf<ArgumentNullException>());
+		}
+
+		[Test]
+		public void Stack_PopEmptyStack_ReturnsInvalidOperationException()
+		{
+			Assert.That(() => _stack.Pop(), Throws.Exception.TypeOf<InvalidOperationException>());
+		}
+
+		[Test]
+		public void Stack_PeekEmptyStack_ReturnsInvalidOperationException()
+		{
+			Assert.That(() => _stack.Peek(), Throws.Exception.TypeOf<InvalidOperationException>());
+		}
+
+		[Test]
+		public void Stack_PushPeekAndPopValidValues_StackBehavesCorrectly()
+		{
+			Assert.That(_stack.Count, Is.EqualTo(0));
+			_stack.Push("a");
+			_stack.Push("b");
+			Assert.That(_stack.Peek(), Is.EqualTo("b"));
+			Assert.That(_stack.Count, Is.EqualTo(2));
+			Assert.That(_stack.Pop(), Is.EqualTo("b"));
+			Assert.That(_stack.Count, Is.EqualTo(1));
+			Assert.That(_stack.Pop(), Is.EqualTo("a"));
+			Assert.That(_stack.Count, Is.EqualTo(0));
+		}
+
+	}
+}
